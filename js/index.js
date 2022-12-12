858821c07267547876280d0ebf01bb2e2729e2d2
igorchagov <igorchagov@mail.ru>
2022-12-12 12:50:26 +0300

    fixed preloader

--- js/index.js
+++ js/index.js
@@ -5,9 +5,10 @@ let newsDate = document.querySelectorAll('.news__date');
 let newsLink = document.querySelectorAll('.news__link');
 let newsContent = document.querySelector('.news__content');
 let preloader = document.querySelector('.news__preloader');
+let newsBox = document.querySelectorAll('.news__box');
 
 async function loadNews() {
-  preloader.style.visibility = 'visible';
+
   let url = `./files/news.json`
   let response = await fetch(url)
   let json = await response.json();
@@ -18,9 +19,21 @@ async function loadNews() {
     newsLink[i].href = json[i].Link;
   }
   preloader.style.visibility = 'hidden';
+  opacityBox()
 }
 
-function openNews(){
+function preloaderActive() {
+  preloader.style.visibility = 'visible';
+}
+
+function opacityBox(){
+  for (let i = 0; i < newsBox.length; i ++){
+    newsBox[i].style.opacity = '1'
+  }
+  
+}
+
+function openNews() {
   newsBtn.classList.toggle('active');
   if (newsBtn.classList.contains('active')) {
     newsContent.style.maxHeight = '5000px'
@@ -30,11 +43,12 @@ function openNews(){
 }
 
 
-newsBtn.addEventListener('click', () => { 
-  if (!newsBtn.classList.contains('active')){
-    loadNews()
+newsBtn.addEventListener('click', () => {
+  if (!newsBtn.classList.contains('active')) {
     openNews()
-  }else{
+    preloaderActive()
+    loadNews()
+  } else {
     openNews()
   }
 })
